e90a79575821d5bcffb8b8ba3c38c3d53e969e0f
Josh <jmaccoby@princeton.edu>
2015-04-11 20:21:59 -0400

    hi

--- Hackprinceton2015/app/src/main/java/com/example/josh/hackprinceton2015/MainActivity.java
+++ Hackprinceton2015/app/src/main/java/com/example/josh/hackprinceton2015/MainActivity.java
@@ -1,17 +1,79 @@
 package com.example.josh.hackprinceton2015;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.provider.MediaStore;
+import java.io.File;
+import java.io.IOException;
+import android.net.Uri;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.os.Environment;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    String mCurrentPhotoPath;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void takePicture(View v) {
+        dispatchTakePictureIntent();
+    }
+
+    public void findMatch(View v) {
+        Toast.makeText(this, mCurrentPhotoPath, Toast.LENGTH_LONG).show();
+        Bitmap bm = BitmapFactory.decodeFile(mCurrentPhotoPath);
+        ImageView i = (ImageView) findViewById(R.id.imageView);
+        i.setImageBitmap(bm);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView i = (ImageView) findViewById(R.id.imageView);
+            i.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 
 
